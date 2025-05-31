a2ffc70008ba55c09833e901cf8992a6440f2924
Tradeprofx <tradeprofx9@gmail.com>
2025-05-31 11:18:42 +0300

    Update app-config.ts

--- packages/shared/src/utils/config/app-config.ts
+++ packages/shared/src/utils/config/app-config.ts
@@ -1,4 +1,4 @@
 export const website_domain = 'app.deriv.com';
-export const website_name = 'Deriv';
+export const website_name = 'TradeProfx';
 export const default_title = 'TradeProfx';
 export const TRACKING_STATUS_KEY = 'tracking_status';
